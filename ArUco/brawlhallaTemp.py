d1a68cdfea86ec3ef3c0f9519ac000c24fca3424
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-08-14 02:09:10 -0700

    Added poetry and added dataset

--- ArUco/brawlhallaTemp.py
+++
@@ -1,4 +0,0 @@
-def printBrawler(num):
-    LegendCorrespondence = ['Ada', 'Orion', 'Zariel', 'Jiro', 'Val', 'Teros', 'Mordex', 'Artemis', 'Lucien', 'Xull',
-                            'Kor', 'Sir Roland']
-    print(LegendCorrespondence[num - 1])
