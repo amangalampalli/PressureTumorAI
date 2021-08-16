69df5c9fe683cdb809a34b6ebe932d1067cc7c59
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-08-16 15:34:44 -0700

    Added tensorflow with GPU support for Mac

--- pressuretumorai/localization/cubeTrack.py
+++ pressuretumorai/localization/cubeTrack.py
@@ -1,18 +1,18 @@
 import cv2
 from cv2 import aruco
-import arucoMarkers
+from pressuretumorai.arucoMarkers.markerGen import markerDict
 import numpy as np
 import pickle
 
 [ret, mtx, dist, rvecs, tvecs] = pickle.load(
-    open("calibration/cameraCoeffs.pickle", "rb")
+    open("pressuretumorai/calibration/cameraCoeffs.pickle", "rb")
 )
 cap = cv2.VideoCapture(0)
 
 
 aruco_dict = markerDict
 parameters = aruco.DetectorParameters_create()
-parameters.cornerRefinementMethod = (cv2.aruco.CORNER_REFINE_SUBPIX,)
+parameters.cornerRefinementMethod = aruco.CORNER_REFINE_SUBPIX
 markerWidth = 0.0508
 
 board_ids = np.array([[0], [1], [2], [3], [4], [5]], dtype=np.int32)
@@ -82,7 +82,7 @@ while True:
     )
 
     if np.all(ids != None):
-        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
+        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, markerWidth, mtx, dist)
         for i in range(len(ids)):
             frame = aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
             frame = aruco.drawDetectedMarkers(frame, corners, ids)
