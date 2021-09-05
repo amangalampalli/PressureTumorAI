c481437d02de006b7514ef07cbedcff281264f59
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-09-04 18:06:46 -0700

    NodeMCU ESP 8266 now sends data over network

--- pressuretumorai/pressure_sensing/data_collection/network_transmission.py
+++ pressuretumorai/pressure_sensing/data_collection/network_transmission.py
@@ -1,34 +1,41 @@
 # type: ignore
-import struct
+
+import machine
+from machine import Pin
 import socket
 import sys
 import random
 import struct
-# from machine import Pin
+import time
+from time import sleep
+
+button = Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
+led = Pin(2, Pin.OUT)
+led.off()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('10.153.34.240', 8000)
+server_address = ('10.0.0.73', 8000)
 print("Connecting to server " + str(server_address))
 sock.connect(server_address)
-# led = Pin(2, Pin.OUT)
-
 
 try:
     print("Connected")
     while True:
-        # led.low()
-        message = random.uniform(0, 100)
-        print("Sending message: " + str(message))
-        sock.send(struct.pack('<f', message))
-        data = sock.recv(4)
-        if data == message:
-            pass
+        if button.value() == 0:
+            led.off()
+            message = float(random.getrandbits(32)/100)
+            print("Sending message: " + str(message))
+            sock.send(struct.pack('<f', message))
+            data = sock.recv(4)
+            if data == message:
+                pass
+            else:
+                Exception("Message not correct")
         else:
-            Exception("Message not correct")
+            led.on()
 
-        # led.high()
-
-
-except KeyboardInterrupt:
+except:
     print("Closing connection")
     sock.close()
+    sys.exit(0)
--- pressuretumorai/pressure_sensing/web_server.py
+++ pressuretumorai/pressure_sensing/web_server.py
@@ -2,28 +2,28 @@ import socket
 import sys
 import struct
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 8000
-server_address = (host, port)
-print("Server starting up at " + str(server_address))
-sock.bind(server_address)
+def startServer():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host = socket.gethostname()
+    port = 8000
+    server_address = (host, port)
+    print("Server starting up at " + str(server_address))
+    sock.bind(server_address)
 
-sock.listen(1)
+    sock.listen(1)
 
-print("Waiting for connection")
-connection, client_address = sock.accept()
+    print("Waiting for connection")
+    connection, client_address = sock.accept()
 
-try:
-    print("Connection from " + str(client_address))
-    while True:
-        data = connection.recv(4)
-        print(struct.unpack("<f", data)[0])
-        connection.sendall(data)
-
-
-except KeyboardInterrupt:
-    connection.close()
-    print("Connection closed")
+    try:
+        print("Connection from " + str(client_address))
+        while True:
+            data = connection.recv(4)
+            print(struct.unpack("<f", data)[0])
+            connection.sendall(data)
+    except:
+        connection.close()
+        print("Connection closed")
     sys.exit()
 
